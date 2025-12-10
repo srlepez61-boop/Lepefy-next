804d46a1264f819455fd5bfc1d2522470915537c
srlepez61-boop <srlepez61@gmail.com>
2025-12-10 15:49:25 -0800

    Create pages.js

---
+++ app/profile/pages.js
@@ -0,0 +1,11 @@
+export default function ProfilePage() {
+  return (
+    <div>
+      <h2 className="page-title">Profile</h2>
+      <p style={{ color: "#b3b3b3" }}>
+        Welcome to Lepefy. In a future version this page can show your stats,
+        favorite artists, playlists, and more.
+      </p>
+    </div>
+  );
+}
